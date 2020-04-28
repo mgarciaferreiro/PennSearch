4e81a63eb3f864387b869ab4aaaf0121246dbc68
peterbaile <peterchenwflms@gmail.com>
2020-04-28 02:30:20 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# PennSearch
-NETS 150 Final Project
+# PennSearch (NETS 150 Final Project)
+
+### Configuration
+`WebCrawler.java` requires the dependency `Jsoup`. To run the code, configure your VScode environment as follows: Click on the left bottom panel of the VScode Editor which says Java Dependencies to expand it, hover on Referenced Libraries and you should able to see a plus sign. Click the plus sign and add `jsoup-1.13.1.jar` file under the `lib` directory.
--- logic/WebCrawler.java
+++ logic/WebCrawler.java
@@ -1,8 +1,76 @@
 package logic;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
- * This class handles crawling through the Penn websites and storing the data in the database.
+ * This class handles crawling through the Penn websites and storing the data in
+ * the database.
+ * 
+ * @author Peter Chen
  */
 public class WebCrawler {
-    // TODO
+    HashSet<String> visitedSet = new HashSet<>();
+    ArrayList<String> ls = new ArrayList<>();
+
+    /**
+     * checks if a URL is Penn-related. This ensures that we are staying
+     * within the Penn network
+     * @param url
+     * @return true is the url is valid, false otherwise
+     */
+    private Boolean isValid(String url) {
+        if (url.contains("http") && url.contains("penn")) return true;
+        return false;
+    }
+
+    /**
+     * define a discover subroutine 
+     * given a source link, add its neighbors to the
+     * visited set and the list if these are not discovered yet
+     */
+    private void discover(String sourceURL) {
+        try {
+            Document doc = Jsoup.connect(sourceURL).get();
+            Elements links = doc.getElementsByTag("a");
+            for (Element link : links) {
+                String url = link.attr("abs:href");
+                if (!visitedSet.contains(url) && isValid(url)) {
+                    ls.add(url);
+                    visitedSet.add(url);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void crawl() {
+        // initialize
+        String startURL = "https://www.upenn.edu/";
+        discover(startURL);
+        System.out.println(visitedSet.size());
+
+        // set a bound on the number of URL to prevent it from running infinitely
+        int maxLinkNo = 200;
+
+        while (!ls.isEmpty() && visitedSet.size() <= maxLinkNo) {
+            String sourceURL = ls.remove(0);
+            System.out.println("the link " + sourceURL + " is popped");
+            discover(sourceURL);
+        }
+
+        for (String l : visitedSet) {
+            System.out.println(l);
+        }
+    }
+
+    public static void main(String[] args) {
+        WebCrawler wc = new WebCrawler();
+    }
 }
