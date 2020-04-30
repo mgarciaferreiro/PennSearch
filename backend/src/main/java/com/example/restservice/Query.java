5148385f86e5b3c469c5481bc1cfe9fa461e3c9d
peterbaile <peterchenwflms@gmail.com>
2020-04-30 15:22:34 -0400

    clean up codebase

--- backend/src/main/java/com/example/restservice/Query.java
+++ backend/src/main/java/com/example/restservice/Query.java
@@ -5,20 +5,14 @@ import logic.*;
 import java.util.List;
 
 public class Query {
-	// private String content;
 	private List<PageSummary> queryAnswers;
 
 	public Query(String query) {
 		Searcher searcher = new Searcher();
 		queryAnswers = searcher.search(query);
-		// this.content = query;
 	}
 
 	public List<PageSummary> getQuery() {
 		return queryAnswers;
 	}
-
-	// public String getContent() {
-	// 	return content;
-	// }
 }
